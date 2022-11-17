5b9832ba3f30a22905f4a36cca2bc956f00c2d08
ALEXANDER <msumba83@gmail.com>
2022-11-17 15:41:19 +0200

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+      <h1 className='main--title'>Fun facts about React</h1>
+      <p>
+        <ul className='main--list'>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+        </ul>
+      </p>
+    </main>
+  );
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../assets/images/aj-2.jpg"
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+       
+          <img src={logo} alt="logo" className='nav--icon'/> 
+          <h1 className='nav--logo--text'>React Facts</h1>
+        
+      <h4 className='nav-title'>React Course Project 1</h4>     
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar;
